f7e30498875b03ae1934d2d2ff6648b92cc10334
Ryan Dhungel <kaloraat@gmail.com>
2018-11-18 12:02:31 +1100

    Updates

---
+++ precache-manifest.8535b92f4c1a263124c46e73d94e397e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e7ea501e7b8f22ce0b7",
+    "url": "/weatherapp/static/js/runtime~main.0e7ea501.js"
+  },
+  {
+    "revision": "b675963a445147d48801",
+    "url": "/weatherapp/static/js/main.b675963a.chunk.js"
+  },
+  {
+    "revision": "f29f7b201744f132a60c",
+    "url": "/weatherapp/static/js/1.f29f7b20.chunk.js"
+  },
+  {
+    "revision": "b675963a445147d48801",
+    "url": "/weatherapp/static/css/main.157289a4.chunk.css"
+  },
+  {
+    "revision": "2495aa99d53d212246449d43f398747a",
+    "url": "/weatherapp/index.html"
+  }
+];
